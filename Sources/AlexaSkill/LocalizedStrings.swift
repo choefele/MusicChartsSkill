d23bd0be63566026b9bfb4c597d209d8e27b209f
Claus Höfele <claus@claushoefele.com>
2016-12-01 18:19:53 +0100

    Add more localized strings

--- Sources/AlexaSkill/LocalizedStrings.swift
+++ Sources/AlexaSkill/LocalizedStrings.swift
@@ -2,11 +2,14 @@ import Foundation
 
 struct LocalizedStrings {
     enum LanguageID: String {
-        case german = "de_DE"
+        case german = "de-DE"
+        case englishUS = "en-US"
+        case englishGB = "en-GB"
     }
 
     enum LocalizationID: String {
         case help
+        case stop
     }
 
     static func localize(_ localizationID: LocalizationID, into languageID: LanguageID) -> String {
@@ -17,7 +20,14 @@ struct LocalizedStrings {
 
     private static let localizedStrings: [LocalizationID: [LanguageID: String]] = [
         .help: [
-            .german: "Hilfe"
+            .german: "Ich kann Dir die beliebtesten Songs auf Spotify nennen. Sag einfach öffne Music Charts",
+            .englishUS: "I can tell you the most popular tracks on Spotify. Just say open Music Charts.",
+            .englishGB: "I can tell you the most popular tracks on Spotify. Just say open Music Charts."
+        ],
+        .stop: [
+            .german: "Bis bald.",
+            .englishUS: "Talk to you soon.",
+            .englishGB: "Talk to you soon."
         ]
     ]
 }
