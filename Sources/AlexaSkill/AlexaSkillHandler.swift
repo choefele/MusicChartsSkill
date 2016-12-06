70f25f6c6db4cdd52ea0dbec5bc0857da8e24ea4
Claus Höfele <claus@claushoefele.com>
2016-12-06 18:34:04 +0100

    Split up functionality into smaller pieces

--- Sources/AlexaSkill/AlexaSkillHandler.swift
+++ Sources/AlexaSkill/AlexaSkillHandler.swift
@@ -9,32 +9,44 @@ public class AlexaSkillHandler : RequestHandler {
     }
     
     public func handleLaunch(request: LaunchRequest, session: Session, next: @escaping (StandardResult) -> ()) {
-        generateResponse(next: next)
+        retrieveCharts(next: next)
     }
     
     public func handleIntent(request: IntentRequest, session: Session, next: @escaping (StandardResult) -> ()) {
-        generateResponse(next: next)
+        retrieveCharts(next: next)
     }
     
     public func handleSessionEnded(request: SessionEndedRequest, session: Session, next: @escaping (VoidResult) -> ()) {
         next(.success())
     }
     
-    func generateResponse(next: @escaping (StandardResult) -> ()) {
+    func retrieveCharts(next: @escaping (StandardResult) -> ()) {
         chartsService.retrieveCharts { result in
-            var message: String
-            if case .success(let entries) = result, entries.count >= 3 {
-                message = "The top three entries in the global Spotify charts are "
-                message += "\(entries[0].trackName) by \(entries[0].artist), "
-                message += "\(entries[1].trackName) by \(entries[1].artist) and "
-                message += "\(entries[2].trackName) by \(entries[2].artist). "
-            } else {
-                message = "Sorry, I'm having troubles finding the Spotify charts right now."
-            }
-
-            let outputSpeech = OutputSpeech.plain(text: message)
-            let standardResponse = StandardResponse(outputSpeech: outputSpeech)
+            let message = self.generateTopArtistsMessage(result: result)
+            let standardResponse = self.generateStandardResponse(message)
             next(.success(standardResponse: standardResponse, sessionAttributes: [:]))
         }
+    }
+
+    func generateTopArtistsMessage(result: ChartsServiceResult<[ChartEntry]>) -> String {
+        var message: String
+        if case .success(let entries) = result, entries.count >= 3 {
+            message = "The top three entries in the global Spotify charts are "
+            message += "\(entries[0].trackName) by \(entries[0].artist), "
+            message += "\(entries[1].trackName) by \(entries[1].artist) and "
+            message += "\(entries[2].trackName) by \(entries[2].artist). "
+        } else {
+            message = "Sorry, I'm having troubles finding the Spotify charts right now."
+        }
+
+
+        return message
+    }
+
+    func generateStandardResponse(_ message: String) -> StandardResponse {
+        let outputSpeech = OutputSpeech.plain(text: message)
+        let standardResponse = StandardResponse(outputSpeech: outputSpeech)
+
+        return standardResponse
     }
 }
